418d7608e6a795a3271952c923d839b71b7fb428
Aparna Nair-Kanneganti <aparna.nair-kanneganti@yale.edu>
2024-04-25 11:51:24 -0400

    typo

--- core_update/sensor.py
+++ core_update/sensor.py
@@ -91,7 +91,7 @@ class Sensor: # sensor points are represented in global coordinate space for thi
     # takes in a list of sensor coords (global coordinate system), transforms to match reference of transmit transducer, and discretizes
     def make_sensor_mask(self, sim_transducer, not_transducer, computational_grid_shape, grid_voxel_size, transmit_transform = None):
         #if self.sensor_coords is None:
-        if type(sim_transducer).__name__ == "Focused" && self.aperture_type == "transmit_as_receive":
+        if type(sim_transducer).__name__ == "Focused" and self.aperture_type == "transmit_as_receive":
             sensor_mask = not_transducer.indexed_mask
             sensor_mask = np.where(sensor_mask > 0, 1, sensor_mask)
             discretized_sensor_coords = None
