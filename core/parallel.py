37348a26185e0c95c6d46cf438ea993dbcca0525
trevor-chan <tjchan0@gmail.com>
2024-05-29 02:14:54 -0400

    edit to parallel args

--- core/parallel.py
+++ core/parallel.py
@@ -20,7 +20,7 @@ def main():
     parser.add_argument('-n', '--node', type=int, help='node index to run')
     parser.add_argument('-s', '--nodes', type=int, help='total number of nodes')
     parser.add_argument('-g', '--gpu', type=bool, default=1, help='whether to use GPU')
-    parser.add_argument('-r', '--results', type=bool, default=0, help='look for results?')
+    parser.add_argument('-r', '--repeat', type=bool, default=1, help='repeat and overwrite?')
     parser.add_argument('-w', '--workers', type=int, default=2, help='number of workers per node')
     
     args = parser.parse_args()
@@ -30,7 +30,7 @@ def main():
     experiment.nodes = args.nodes
     experiment.gpu = args.gpu
     experiment.workers = args.workers
-    if args.results:
+    if not args.repeat:
         experiment.indices = experiment.indices_to_run()
     experiment.run(args.node)
     
